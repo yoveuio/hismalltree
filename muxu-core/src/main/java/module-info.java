1f630930327096f407795f23060c09495f6de0a0
fangpeiyu <fangpeiyu.it@gmail.com>
2023-10-15 17:56:07 +0800

    refactor: modularity muxu-core module

---
+++ muxu-core/src/main/java/module-info.java
@@ -0,0 +1,11 @@
+module demo.muxu.core {
+
+    exports com.muxu.core.utils;
+
+    requires kotlin.stdlib;
+    requires com.fasterxml.jackson.core;
+    requires com.google.common;
+    requires typesafe.config;
+    requires org.yaml.snakeyaml;
+
+}
