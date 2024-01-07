bbffe3aac1769ce16137583aaf125828c2843421
fangpeiyu <fangpeiyu.it@gmail.com>
2024-01-07 17:13:55 +0800

    demo: flink watermark demo

--- demo/demo-flink-scala/src/main/scala/com/hismalltree/demo/flink/Demo.scala
+++ demo/demo-flink-scala/src/main/scala/com/hismalltree/demo/flink/Demo.scala
@@ -2,7 +2,7 @@ package com.hismalltree.demo.flink
 
 import com.fasterxml.jackson.annotation.JsonGetter
 import org.apache.flink.api.common.eventtime.{WatermarkGenerator, WatermarkGeneratorSupplier, WatermarkStrategy}
-import org.apache.flink.api.common.functions.MapFunction
+import org.apache.flink.api.common.functions.{AggregateFunction, MapFunction}
 import org.apache.flink.configuration.Configuration
 import org.apache.flink.streaming.api.datastream.DataStream
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment
@@ -25,13 +25,14 @@ object Demo {
     val persons: DataStream[Person] = numbers.map(new Tuple2ToPersonMapper())
       .assignTimestampsAndWatermarks(new CustomWatermarksStrategy)
 
+    persons.print()
+    env.execute()
   }
 }
 
 class Tuple2ToPersonMapper extends MapFunction[(String, Int), Person] {
 
   override def map(value: (String, Int)): Person = Person(name = value._1, age = value._2)
-
 
 }
 
@@ -50,4 +51,22 @@ class CustomWatermarksStrategy extends WatermarkStrategy[Person] {
     ???
   }
 
+}
+
+class AvgTempFunction extends AggregateFunction[(String, Double), (String, Double, Int), (String, Double)] {
+  override def createAccumulator(): (String, Double, Int) = {
+    ("", 0.0, 0)
+  }
+
+  override def add(value: (String, Double), accumulator: (String, Double, Int)): (String, Double, Int) = {
+    (value._1, value._2 + accumulator._2, 1 + accumulator._3)
+  }
+
+  override def getResult(accumulator: (String, Double, Int)): (String, Double) = {
+    (accumulator._1, accumulator._2 / accumulator._3)
+  }
+
+  override def merge(a: (String, Double, Int), b: (String, Double, Int)): (String, Double, Int) = {
+    (a._1, a._2 + b._2, a._3 + b._3)
+  }
 }
